76ef798b2233f2fab2d156863d0b0d8801fbe245
Dylan Warren <dylanwarren19@gmail.com>
2023-04-08 23:13:46 -0500

    update: routing for app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,8 @@
 import { Component } from '@angular/core';
-import { invoke } from '@tauri-apps/api/tauri';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  greetingMessage = '';
-
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
-
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    invoke<string>('greet', { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-
-    invoke<string>('play_tts', {
-      url: 'https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=pog%20online%20poggers',
-    }).catch(console.error);
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
