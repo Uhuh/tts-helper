7e32abe95ea1ae85789d4fa7e88ef87991ed4d4e
Dylan Warren <dylanwarren19@gmail.com>
2023-04-08 23:12:53 -0500

    feat: preparing to make styled material components

---
+++ src/app/shared/components/toggle/toggle.component.html
@@ -0,0 +1,1 @@
+<p>toggle works!</p>
---
+++ src/app/shared/components/toggle/toggle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.scss']
+})
+export class ToggleComponent {
+
+}
