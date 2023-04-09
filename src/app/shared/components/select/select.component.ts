91807d264f90a14da8ecc3c390b76fab6928cedc
Dylan Warren <dylanwarren19@gmail.com>
2023-04-09 16:13:53 -0500

    update: slightly styled select

--- src/app/shared/components/select/select.component.ts
+++ src/app/shared/components/select/select.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.scss']
+  styleUrls: ['./select.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class SelectComponent {
-
+  @Input() control!: FormControl<string>;
+  @Input() placeholder = 'Select an option';
 }
--- src/app/shared/components/select/select.module.ts
+++ src/app/shared/components/select/select.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectComponent } from './select.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SelectComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+  ],
   exports: [SelectComponent],
 })
 export class SelectModule {}
