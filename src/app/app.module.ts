a4d080b73a9ef03334a3973fd89f6e0a3da84c64
Dylan Warren <dylanwarren19@gmail.com>
2023-04-10 01:02:15 -0500

    update: small visual changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavModule } from './shared/components/nav/nav.module';
+import { StoreModule } from '@ngrx/store';
+import { historyReducer } from './shared/state/history/history.reducers';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot({ historyState: historyReducer }),
     BrowserAnimationsModule,
     NavModule,
   ],
