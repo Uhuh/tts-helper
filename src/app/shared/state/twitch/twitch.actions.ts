e4b96f10e86f64d6302ab93e7c53e425456756bd
Dylan Warren <dylanwarren19@gmail.com>
2023-04-15 04:05:29 -0500

    update: twitch state actions

---
+++ src/app/shared/state/twitch/twitch.actions.ts
@@ -0,0 +1,15 @@
+﻿import { createAction, props } from "@ngrx/store";
+import { TwitchChannelInfo } from "./twitch.interface";
+
+export const updateToken = createAction(
+  '[ TwitchState ] Updating token',
+  props<{ token: string | null }>(),
+);
+export const updateTokenValidity = createAction(
+  '[ TwitchState ] Updating token validity',
+  props<{ isTokenValid: boolean }>(),
+)
+export const updateChannelInfo = createAction(
+  '[ TwitchState ] Updating channel info',
+  props<{ channelInfo: TwitchChannelInfo }>(),
+);
