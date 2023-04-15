5d9649d304ce536f80e8082373f640cb761cfcee
Dylan Warren <dylanwarren19@gmail.com>
2023-04-15 04:04:09 -0500

    feat: show user twitch connection status and ability to "logout"

--- src/app/pages/twitch/twitch.component.ts
+++ src/app/pages/twitch/twitch.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { TwitchService } from "../../shared/services/twitch.service";
+import { listen } from "@tauri-apps/api/event";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-twitch',
   templateUrl: './twitch.component.html',
   styleUrls: ['./twitch.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class TwitchComponent implements OnInit, OnDestroy {
   private readonly destroyed$ = new Subject<void>();
@@ -15,10 +19,48 @@ export class TwitchComponent implements OnInit, OnDestroy {
     'channel%3Aread%3Aredemptions+channel%3Aread%3Asubscriptions+chat%3Aread';
   readonly loginUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${this.clientId}&redirect_uri=${this.redirect}&response_type=token&scope=${this.scopes}`;
 
-  constructor() {}
+  isTokenValid = false;
+  constructor(
+    private readonly twitchService: TwitchService,
+    private readonly snackbar: MatSnackBar,
+    private readonly ref: ChangeDetectorRef,
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.twitchService.isTokenValid$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(isTokenValid => {
+        this.isTokenValid = isTokenValid;
+        this.ref.detectChanges();
+      });
+    
+    listen('access-token', (authData) => {
+      const { token, provider } = authData.payload as { token: string, provider: 'twitch' | 'youtube' };
 
+      if (provider !== 'twitch') {
+        return;
+      }
+
+      this.twitchService.updateToken(token);
+    })
+      .catch(e => {
+        console.error('Encountered issue getting access token.', e);
+
+        this.snackbar.open(
+          'Oops! We encountered an error while authorizing...',
+          'Dismiss',
+          {
+            panelClass: 'notification-error',
+          }
+        );
+      });
+  }
+
+  signOut() {
+    this.twitchService.signOut();
+    this.ref.detectChanges();
+  }
+  
   ngOnDestroy(): void {
     this.destroyed$.next();
     this.destroyed$.complete();
---
+++ src/app/shared/state/twitch/twitch.interface.ts
@@ -0,0 +1,11 @@
+﻿export interface TwitchChannelInfo {
+  channelId: string;
+  username: string;
+}
+
+export interface ValidUser {
+  user_id: string;
+  login: string;
+  scopes: string[];
+  expires_in: number;
+}
