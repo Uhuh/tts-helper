94d5a98470d61e8d4417eb0618ac05aa2e741dcf
Dylan Warren <dylanwarren19@gmail.com>
2023-04-16 02:08:31 -0500

    update: sidenav cleanup

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,32 +1,8 @@
 import { Component } from '@angular/core';
 
-export interface SideNavLink {
-  name: string;
-  url: string;
-}
-
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
 })
-export class SidenavComponent {
-  links: SideNavLink[] = [
-    {
-      name: 'Home',
-      url: 'home',
-    },
-    {
-      name: 'History',
-      url: 'history',
-    },
-    {
-      name: 'Twitch',
-      url: 'twitch',
-    },
-    {
-      name: 'Youtube',
-      url: 'youtube',
-    },
-  ];
-}
+export class SidenavComponent {}
