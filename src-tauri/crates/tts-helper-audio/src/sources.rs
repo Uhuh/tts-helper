b62a6d195706961b9a15294c163ba7fefaf9fc7d
TehPers <tehperz@gmail.com>
2023-04-18 21:13:30 -0700

    Rename file

--- src-tauri/crates/tts-helper-audio/src/sources.rs
+++ src-tauri/crates/tts-helper-audio/src/sources.rs
@@ -1,11 +1,11 @@
 mod on_finish;
 mod on_start;
-mod skip_if;
+mod skip_when;
 mod source_ext;
 mod take_for;
 
 pub use on_finish::*;
 pub use on_start::*;
-pub use skip_if::*;
+pub use skip_when::*;
 pub use source_ext::*;
 pub use take_for::*;
