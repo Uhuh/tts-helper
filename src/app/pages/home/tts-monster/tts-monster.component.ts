9b7be0f02e8c13180cb2965e4e5744e63cd6858f
Dylan Warren <dylanwarren19@gmail.com>
2023-04-20 00:45:06 -0500

    update: form group for tts-monster

--- src/app/pages/home/tts-monster/tts-monster.component.ts
+++ src/app/pages/home/tts-monster/tts-monster.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { nonNullFormControl } from 'src/app/shared/utils/form';
 
 @Component({
