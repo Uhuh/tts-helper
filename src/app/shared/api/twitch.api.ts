7aabf0e9f7045255678e49c9ed5a7a65dd6880f8
Dylan Warren <dylanwarren19@gmail.com>
2023-04-21 03:13:48 -0500

    update: move most api logic to service

--- src/app/shared/api/twitch.api.ts
+++ src/app/shared/api/twitch.api.ts
@@ -1,16 +1,10 @@
-﻿import { Inject, Injectable } from '@angular/core';
+﻿import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { LOCAL_STORAGE } from '../tokens/localStorage.token';
 import { Observable } from 'rxjs';
-import {
-  updateChannelRedeems,
-  updateTwitchState,
-} from '../state/twitch/twitch.actions';
-import { Store } from '@ngrx/store';
-import { TwitchRedeemInfo, ValidUser } from '../state/twitch/twitch.interface';
+
 import { TwitchRedeem } from './twitch.interface';
 import { OnDestroy } from '@angular/core';
-import { switchMap, Subject, takeUntil } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class TwitchApi implements OnDestroy {
@@ -18,22 +12,8 @@ export class TwitchApi implements OnDestroy {
   private readonly apiUrl = 'https://api.twitch.tv/helix';
   private readonly url = 'https://id.twitch.tv';
   private readonly clientId = 'fprxp4ve0scf8xg6y48nwcq1iogxuq';
-  private readonly storageKey = 'twitchAccessToken';
-  private headers: HttpHeaders;
 
-  constructor(
-    @Inject(LOCAL_STORAGE) private readonly localStorage: Storage,
-    private readonly http: HttpClient,
-    private readonly store: Store
-  ) {
-    this.headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: `OAuth ${this.localStorage.getItem(this.storageKey)}`,
-      'Client-Id': this.clientId,
-    });
-
-    this.getTokenFromStorage();
-  }
+  constructor(private readonly http: HttpClient) {}
 
   ngOnDestroy(): void {
     this.destroyed$.next();
@@ -46,123 +26,34 @@ export class TwitchApi implements OnDestroy {
    * @returns Array of redeems that belong to the broadcaster id.
    */
   getChannelRedeemCommands(
-    broadcasterId: string
+    broadcaster_id: string,
+    token: string
   ): Observable<{ data: TwitchRedeem[] }> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${this.localStorage.getItem(this.storageKey)}`,
+      Authorization: `Bearer ${token}`,
       'Client-Id': this.clientId,
     });
 
-    return this.http.get<{ data: any[] }>(
+    return this.http.get<{ data: TwitchRedeem[] }>(
       `${this.apiUrl}/channel_points/custom_rewards`,
       {
         headers,
         params: {
-          broadcaster_id: broadcasterId,
+          broadcaster_id,
         },
       }
     );
   }
 
-  getTokenFromStorage() {
-    const token = this.localStorage.getItem(this.storageKey);
-
-    this.headers = new HttpHeaders({
-      Authorization: `OAuth ${token}`,
-    });
-
-    // When launching we need to verify the users token is still valid. Who knows how long they waited.
-    this.handleValidateToken(token);
-  }
-
-  updateStorage(token: string | null) {
-    if (token) {
-      this.localStorage.setItem(this.storageKey, token);
-    } else {
-      this.localStorage.removeItem(this.storageKey);
-    }
-  }
-
-  updateToken(token: string | null) {
-    this.headers = new HttpHeaders({
+  validateToken(token: string): Observable<any> {
+    const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       Authorization: `OAuth ${token}`,
     });
 
-    return this.handleValidateToken(token);
-  }
-
-  handleValidateToken(token: string | null) {
-    return this.validateToken()
-      .pipe(
-        takeUntil(this.destroyed$),
-        switchMap((validUser: ValidUser) => {
-          this.updateStorage(token);
-
-          const twitchState = {
-            isTokenValid: true,
-            token,
-            redeem: null,
-            redeemCharacterLimit: 300,
-            channelInfo: {
-              channelId: validUser.user_id,
-              username: validUser.login,
-              redeems: [],
-            },
-          };
-
-          this.store.dispatch(
-            updateTwitchState({
-              twitchState,
-            })
-          );
-
-          return this.getChannelRedeemCommands(validUser.user_id);
-        })
-      )
-      .subscribe({
-        next: (twitchRedeems) => {
-          const redeems = twitchRedeems.data
-            .filter((r) => r.is_enabled)
-            .map<TwitchRedeemInfo>((r) => ({
-              cost: r.cost ?? 0,
-              id: r.id,
-              prompt: r.prompt,
-              title: r.title,
-            }));
-
-          this.store.dispatch(updateChannelRedeems({ redeems }));
-        },
-
-        error: (err) => {
-          console.error('Failed to validate users access token.', err);
-          this.updateStorage(null);
-
-          const twitchState = {
-            isTokenValid: false,
-            token: null,
-            redeem: null,
-            redeemCharacterLimit: 300,
-            channelInfo: {
-              channelId: '',
-              username: '',
-              redeems: [],
-            },
-          };
-
-          this.store.dispatch(
-            updateTwitchState({
-              twitchState,
-            })
-          );
-        },
-      });
-  }
-
-  validateToken(): Observable<ValidUser> {
-    return this.http.get<ValidUser>(`${this.url}/oauth2/validate`, {
-      headers: this.headers,
+    return this.http.get<any>(`${this.url}/oauth2/validate`, {
+      headers,
     });
   }
 }
--- src/app/shared/services/twitch.service.ts
+++ src/app/shared/services/twitch.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   selectIsTokenValid,
@@ -12,12 +12,21 @@ import {
 import { TwitchApi } from '../api/twitch.api';
 import {
   updateChannelInfo,
+  updateChannelRedeems,
   updateRedeem,
   updateRedeemCharLimit,
+  updateToken,
+  updateTokenValidity,
+  updateTwitchState,
 } from '../state/twitch/twitch.actions';
+import { Subject, interval, switchMap, takeUntil } from 'rxjs';
+import { listen } from '@tauri-apps/api/event';
+import { ValidUser } from '../state/twitch/twitch.interface';
 
 @Injectable()
-export class TwitchService {
+export class TwitchService implements OnDestroy {
+  private readonly destroyed$ = new Subject<void>();
+
   public readonly twitchState$ = this.store.select(selectTwitchState);
   public readonly twitchToken$ = this.store.select(selectTwitchToken);
   public readonly isTokenValid$ = this.store.select(selectIsTokenValid);
@@ -29,27 +38,85 @@ export class TwitchService {
   constructor(
     private readonly store: Store,
     private readonly twitchApi: TwitchApi
-  ) {}
+  ) {
+    this.twitchToken$.pipe(takeUntil(this.destroyed$)).subscribe((token) => {
+      /**
+       * @TODO - If token is not null, setup validation.
+       */
+    });
 
-  signOut() {
-    this.twitchApi.updateToken('');
+    listen('access-token', (authData) => {
+      const { token, provider } = authData.payload as {
+        token: string;
+        provider: 'twitch' | 'youtube';
+      };
+
+      if (provider !== 'twitch') {
+        return;
+      }
+
+      this.signIn(token);
+    }).catch((e) => {
+      console.error('Encountered issue getting access token.', e);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+
+  clearState() {
     this.store.dispatch(
-      updateChannelInfo({
-        channelInfo: {
-          channelId: '',
-          username: '',
-          redeems: [],
+      updateTwitchState({
+        twitchState: {
+          isTokenValid: false,
+          token: null,
+          redeem: null,
+          redeemCharacterLimit: 300,
+          channelInfo: {
+            channelId: '',
+            username: '',
+            redeems: [],
+          },
         },
       })
     );
   }
 
-  getChannelRedeemCommands(broadcasterId: string) {
-    return this.twitchApi.getChannelRedeemCommands(broadcasterId);
+  signOut() {
+    this.clearState();
   }
 
-  updateToken(token: string) {
-    return this.twitchApi.updateToken(token);
+  signIn(token: string) {
+    this.twitchApi
+      .validateToken(token)
+      .pipe(
+        takeUntil(this.destroyed$),
+        switchMap((user: ValidUser) => {
+          console.info(user);
+
+          this.store.dispatch(updateToken({ token }));
+          this.store.dispatch(updateTokenValidity({ isTokenValid: true }));
+          this.store.dispatch(
+            updateChannelInfo({
+              channelInfo: {
+                username: user.login,
+                channelId: user.user_id,
+                redeems: [],
+              },
+            })
+          );
+
+          return this.twitchApi.getChannelRedeemCommands(user.user_id, token);
+        })
+      )
+      .subscribe({
+        next: (data) => {
+          this.store.dispatch(updateChannelRedeems({ redeems: data.data }));
+        },
+        error: (e) => console.error(e),
+      });
   }
 
   updateSelectedRedeem(redeem: string | null) {
