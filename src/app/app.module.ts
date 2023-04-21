286295131d7aceb8ea6f968d59e9ce0c882dc147
Dylan Warren <dylanwarren19@gmail.com>
2023-04-21 03:15:40 -0500

    update: add storage service to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
@@ -16,6 +16,8 @@ import { TwitchPubSub } from './shared/services/twitch-pubsub';
 import { HistoryService } from './shared/services/history.service';
 import { ConfigService } from './shared/services/config.service';
 import { configReducer } from './shared/state/config/config.reducers';
+import { StorageService } from './shared/services/storage.service';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
@@ -31,11 +33,13 @@ import { configReducer } from './shared/state/config/config.reducers';
     BrowserAnimationsModule,
     MatSnackBarModule,
     NavModule,
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [
     TwitchService,
     TwitchApi,
     TwitchPubSub,
+    StorageService,
     ConfigService,
     HistoryService,
     {
