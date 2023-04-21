93001dfc47aa32cc7e2eb574cf98e438793daa34
Dylan Warren <dylanwarren19@gmail.com>
2023-04-21 03:51:45 -0500

    feat: allow users to pause queue

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { invoke } from '@tauri-apps/api';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss'],
 })
-export class HistoryComponent {}
+export class HistoryComponent {
+  isPaused = false;
+
+  togglePause() {
+    this.isPaused = !this.isPaused;
+
+    invoke('set_tts_paused', {
+      paused: this.isPaused,
+    }).catch(() => {
+      console.error('Failed to pause audio');
+    });
+  }
+}
--- src/app/pages/history/history.module.ts
+++ src/app/pages/history/history.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HistoryComponent } from './history.component';
 import { HistoryListModule } from 'src/app/shared/components/history/history-list.module';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'src/app/shared/components/button/button.module';
 
 @NgModule({
   declarations: [HistoryComponent],
   imports: [
     CommonModule,
+    ButtonModule,
     HistoryListModule,
     RouterModule.forChild([
       {
