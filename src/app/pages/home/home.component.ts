8cca3aee59b4f14a4849753dbcc6697addea3f77
Dylan Warren <dylanwarren19@gmail.com>
2023-04-23 03:16:46 -0500

    update: clean up home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,62 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AuditItem } from 'src/app/shared/state/history/history-item.interface';
-import { HistoryService } from 'src/app/shared/services/history.service';
-import { nonNullFormControl } from 'src/app/shared/utils/form';
-import { ConfigService } from 'src/app/shared/services/config.service';
-import { Subject, takeUntil } from 'rxjs';
-import { TtsType } from 'src/app/shared/state/config/config.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  private readonly destroyed$ = new Subject<void>();
-  ttsControl = nonNullFormControl('');
-  selectedTts = nonNullFormControl<TtsType>('stream-elements');
-  history: AuditItem[] = [];
-
-  constructor(
-    private readonly historyService: HistoryService,
-    private readonly configService: ConfigService
-  ) {}
-
-  ngOnInit(): void {
-    this.configService.voiceSettingsTts$
-      .pipe(takeUntil(this.destroyed$))
-      .subscribe((tts) => {
-        this.selectedTts.patchValue(tts, {
-          emitEvent: false,
-        });
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.destroyed$.next();
-    this.destroyed$.complete();
-  }
-
-  selectTts(tts: TtsType) {
-    this.selectedTts.setValue(tts);
-
-    this.configService.updateTts(tts);
-    this.configService.updateUrl(tts);
-  }
-
-  speak(): void {
-    const { value } = this.ttsControl;
-    this.ttsControl.setValue('');
-
-    this.historyService.playTts(
-      value ?? 'Oops no rizz!',
-      '',
-      'tts-helper',
-      1000
-    );
-  }
-
-  get isDisabled() {
-    return this.ttsControl.value === '';
-  }
-}
+export class HomeComponent {}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,39 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
-import { InputModule } from '../../shared/components/input/input.module';
-import { ButtonModule } from '../../shared/components/button/button.module';
-import { ToggleModule } from '../../shared/components/toggle/toggle.module';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatSelectModule } from '@angular/material/select';
-import { ModerationComponent } from './moderation/moderation.component';
-import { TtsMonsterComponent } from './tts-monster/tts-monster.component';
-import { StreamelementTtsComponent } from './streamelement-tts/streamelement-tts.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    ModerationComponent,
-    TtsMonsterComponent,
-    StreamelementTtsComponent,
-  ],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
-    InputModule,
-    MatFormFieldModule,
-    FormsModule,
-    ButtonModule,
-    ToggleModule,
-    RouterModule,
     RouterModule.forChild([
       {
         path: '',
         component: HomeComponent,
       },
     ]),
-    ReactiveFormsModule,
-    MatSelectModule,
   ],
   exports: [HomeComponent],
 })
