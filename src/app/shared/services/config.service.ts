55aa510411d4d39047dad473b6482a02d25a4c97
Dylan Warren <dylanwarren19@gmail.com>
2023-04-23 02:16:40 -0500

    update: handle updating config state

--- src/app/shared/services/config.service.ts
+++ src/app/shared/services/config.service.ts
@@ -4,16 +4,23 @@ import {
   selectBannedWords,
   selectConfigState,
   selectVoiceSettings,
+  selectVoiceSettingsTts,
 } from '../state/config/config.selectors';
 import {
   updateBannedWords,
   updateConfigState,
+  updateLanguage,
+  updateTts,
+  updateUrl,
+  updateVoice,
 } from '../state/config/config.actions';
+import { TtsType, TtsUrlMap } from '../state/config/config.interface';
 
 @Injectable()
 export class ConfigService {
   public readonly configState$ = this.store.select(selectConfigState);
   public readonly voiceSettings$ = this.store.select(selectVoiceSettings);
+  public readonly voiceSettingsTts$ = this.store.select(selectVoiceSettingsTts);
   public readonly bannedWords$ = this.store.select(selectBannedWords);
 
   /**
@@ -30,13 +37,31 @@ export class ConfigService {
     this.store.dispatch(updateBannedWords({ bannedWords: words }));
   }
 
+  updateLanguage(language: string) {
+    this.store.dispatch(updateLanguage({ language }));
+  }
+
+  updateVoice(voice: string) {
+    this.store.dispatch(updateVoice({ voice }));
+  }
+
+  updateTts(tts: TtsType) {
+    this.store.dispatch(updateTts({ tts }));
+  }
+
+  updateUrl(tts: TtsType) {
+    this.store.dispatch(updateUrl({ url: TtsUrlMap[tts] }));
+  }
+
   clearState() {
     this.store.dispatch(
       updateConfigState({
         configState: {
           bannedWords: [],
           voiceSettings: {
-            url: '',
+            tts: 'stream-elements',
+            url: 'https://api.streamelements.com/kappa/v2/speech',
+            language: '',
             voice: '',
             voiceQueryParam: '',
           },
