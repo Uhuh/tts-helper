664bf8058b79c70d864744612c0de17497be8ccb
Dylan Warren <dylanwarren19@gmail.com>
2023-04-23 02:14:53 -0500

    update: remove child routes

--- src/app/pages/home/tts-router/tts-router.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'app-tts-router',
-  templateUrl: './tts-router.component.html',
-  styleUrls: ['./tts-router.component.scss'],
-  encapsulation: ViewEncapsulation.None,
-})
-export class TtsRouterComponent {}
