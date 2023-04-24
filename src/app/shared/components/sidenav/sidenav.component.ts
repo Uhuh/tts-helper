fe21c846639f123ba2db93b4667c603c51c34b9f
Dylan Warren <dylanwarren19@gmail.com>
2023-04-24 14:42:01 -0500

    update: display version at bottom of sidenav

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { getVersion } from '@tauri-apps/api/app';
 
 @Component({
   selector: 'app-sidenav',
@@ -9,6 +10,11 @@ import { MatSidenav } from '@angular/material/sidenav';
 export class SidenavComponent {
   @Input() nav!: MatSidenav;
   @Input() isMobile = false;
+  appVersion: string = '';
+
+  constructor() {
+    getVersion().then((v) => (this.appVersion = v));
+  }
 
   close() {
     if (!this.isMobile) return;
