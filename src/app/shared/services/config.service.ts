fcc402cf6ff606ff22e33b36a55f05041a1ba366
Dylan Warren <dylanwarren19@gmail.com>
2023-04-27 22:21:33 -0500

    update: users can now user tts monster

--- src/app/shared/services/config.service.ts
+++ src/app/shared/services/config.service.ts
@@ -3,6 +3,8 @@ import { Store } from '@ngrx/store';
 import {
   selectBannedWords,
   selectConfigState,
+  selectStreamElements,
+  selectTtsMonster,
   selectVoiceSettings,
   selectVoiceSettingsTts,
 } from '../state/config/config.selectors';
@@ -11,6 +13,7 @@ import {
   updateConfigState,
   updateLanguage,
   updateTts,
+  updateTtsMonsterOverlayInfo,
   updateUrl,
   updateVoice,
 } from '../state/config/config.actions';
@@ -20,6 +23,8 @@ import { TtsType, TtsUrlMap } from '../state/config/config.interface';
 export class ConfigService {
   public readonly configState$ = this.store.select(selectConfigState);
   public readonly voiceSettings$ = this.store.select(selectVoiceSettings);
+  public readonly streamElements$ = this.store.select(selectStreamElements);
+  public readonly ttsMonster$ = this.store.select(selectTtsMonster);
   public readonly voiceSettingsTts$ = this.store.select(selectVoiceSettingsTts);
   public readonly bannedWords$ = this.store.select(selectBannedWords);
 
@@ -42,6 +47,14 @@ export class ConfigService {
     this.store.dispatch(updateVoice({ voice }));
   }
 
+  updateTtsMonsterOverlayInfo(partial: {
+    overlay: string;
+    userId: string;
+    key: string;
+  }) {
+    this.store.dispatch(updateTtsMonsterOverlayInfo({ ...partial }));
+  }
+
   updateTts(tts: TtsType) {
     this.store.dispatch(updateTts({ tts }));
   }
@@ -58,9 +71,20 @@ export class ConfigService {
           voiceSettings: {
             tts: 'stream-elements',
             url: 'https://api.streamelements.com/kappa/v2/speech',
-            language: '',
-            voice: '',
-            voiceQueryParam: '',
+            streamElements: {
+              language: '',
+              voice: '',
+            },
+            ttsMonster: {
+              overlay: '',
+              userId: '',
+              key: '',
+              message: '',
+              ai: false,
+              details: {
+                provider: 'tts-helper',
+              },
+            },
           },
         },
       })
