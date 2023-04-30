826a1f9df7761918f5cc50c751a0cc882a9048e6
Dylan Warren <dylanwarren19@gmail.com>
2023-04-30 03:30:51 -0500

    update: twitch sub event types

--- src/app/shared/services/twitch-pubsub.interface.ts
+++ src/app/shared/services/twitch-pubsub.interface.ts
@@ -22,7 +22,45 @@ export interface TwitchCheer {
   broadcasterName: string;
   isAnonymous: boolean;
   message: string;
-  userId: string;
+  userId: string | null;
+  userName: string | null;
+  userDisplayName: string | null;
+}
+
+export interface TwitchSub {
+  broadcasterDisplayName: string;
+  broadcasterName: string;
+  broadcasterId: string;
+  isGift?: boolean;
+  tier: string;
   userDisplayName: string;
+  userId: string;
   userName: string;
+}
+
+export interface TwitchGiftSub {
+  broadcasterDisplayName: string;
+  broadcasterId: string;
+  broadcasterName: string;
+  cumulativeAmount: number | null;
+  amount: number;
+  gifterId: string;
+  gifterName: string;
+  gifterDisplayName: string;
+  isAnonymous: boolean;
+  tier: string;
+}
+
+export interface TwitchSubMessage {
+  broadcasterDisplayName: string;
+  broadcasterId: string;
+  broadcasterName: string;
+  cumulativeMonths: number;
+  streakMonths: number | null;
+  durationMonths: number;
+  tier: string;
+  userId: string;
+  userName: string;
+  userDisplayName: string;
+  messageText: string;
 }
