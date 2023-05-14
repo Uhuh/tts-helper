bddb5dd2a6dfacc9a787b4e6db0e7738e00c5558
TehPers <tehperz@gmail.com>
2023-05-14 01:16:29 -0700

    Remove audio from main project

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -1,11 +1,3 @@
-mod audio_request;
-mod audio_state;
 mod auth_event;
-mod devices;
-mod playing_audio;
 
-pub use audio_request::*;
-pub use audio_state::*;
 pub use auth_event::*;
-pub use devices::*;
-pub use playing_audio::*;
--- src-tauri/src/services.rs
+++ src-tauri/src/services.rs
@@ -1,9 +1,3 @@
-mod devices;
-mod now_playing;
 mod oauth_listener;
-mod player;
 
-pub use devices::*;
-pub use now_playing::*;
 pub use oauth_listener::*;
-pub use player::*;
