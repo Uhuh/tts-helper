888384cefebb4cf6e7d5b1b5cda08580b3632ed6
Dylan Warren <dylanwarren19@gmail.com>
2023-06-15 22:35:29 -0500

    chore: figuring out why webview is breaking rendering

--- src/app/pages/history/history-list/history-list.component.ts
+++ src/app/pages/history/history-list/history-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NgIf, NgFor } from '@angular/common';
 import { AuditItem } from '../../../shared/state/history/history-item.interface';
@@ -14,8 +14,8 @@ import { HistoryItemComponent } from '../history-item/history-item.component';
   imports: [NgIf, HistoryItemComponent, NgFor],
 })
 export class HistoryListComponent {
-  items: AuditItem[] = [];
-  currentlyPlaying?: AuditItem;
+  items = signal<AuditItem[]>([]);
+  currentlyPlaying = signal<AuditItem | undefined>(undefined);
 
   constructor(
     private readonly playbackService: PlaybackService,
@@ -25,23 +25,21 @@ export class HistoryListComponent {
     this.playbackService.audioStarted$
       .pipe(takeUntilDestroyed())
       .subscribe((id) => {
-        this.currentlyPlaying = this.items.find((i) => i.id === id);
+        this.currentlyPlaying.set(this.items().find((i) => i.id === id));
         // It will absolutely not display in time without this.
-        this.ref.markForCheck();
       });
 
     this.playbackService.audioFinished$
       .pipe(takeUntilDestroyed())
       .subscribe((id) => {
-        if (this.currentlyPlaying?.id !== id) return;
-        this.currentlyPlaying = undefined;
+        if (this.currentlyPlaying()?.id !== id) return;
+        this.currentlyPlaying.set(undefined);
       });
 
     this.historyService.auditItems$
       .pipe(takeUntilDestroyed())
       .subscribe((items) => {
-        this.items = items;
-        this.ref.markForCheck();
+        this.items.set(items);
       });
   }
 }
