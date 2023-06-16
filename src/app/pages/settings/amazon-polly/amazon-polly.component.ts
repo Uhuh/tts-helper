fa8bd2b296a9662802c0f8fcbc4ae990ce043582
Dylan Warren <dylanwarren19@gmail.com>
2023-06-15 21:29:04 -0500

    update: use new tts-selector component

--- src/app/pages/settings/amazon-polly/amazon-polly.component.ts
+++ src/app/pages/settings/amazon-polly/amazon-polly.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { ConfigService } from '../../../shared/services/config.service';
 import { nonNullFormControl } from '../../../shared/utils/form';
 import voices from '../../../shared/json/amazon-polly.json';
@@ -9,11 +9,7 @@ import { NgFor } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
-export interface LanguageOptions {
-  value: string;
-  displayName: string;
-}
+import { TtsSelectorComponent } from '../../../shared/components/tts-selector/tts-selector.component';
 
 @Component({
   selector: 'app-amazon-polly',
@@ -28,13 +24,12 @@ export interface LanguageOptions {
     NgFor,
     MatOptionModule,
     InputComponent,
+    TtsSelectorComponent,
   ],
 })
 export class AmazonPollyComponent {
   regions = [...voices.regions];
-  languageVoiceMap = new Map<string, LanguageOptions[]>();
-  languageVoiceOptions = signal<LanguageOptions[]>([]);
-  languageOptions = signal<string[]>([]);
+  readonly voices = voices.options;
 
   regionControl = nonNullFormControl(this.regions[0]);
   poolIdControl = nonNullFormControl('');
@@ -42,26 +37,12 @@ export class AmazonPollyComponent {
   voiceControl = nonNullFormControl('');
 
   constructor(private readonly configService: ConfigService) {
-    for (const voice of voices.voices) {
-      this.languageVoiceMap.set(voice.language, voice.voices);
-    }
-
-    this.languageOptions.set([...this.languageVoiceMap.keys()]);
-
-    this.languageVoiceOptions.set(
-      this.languageVoiceMap.get(this.languageOptions()[0]) ?? []
-    );
-
     this.configService.amazonPolly$
       .pipe(takeUntilDestroyed())
       .subscribe((amazonPolly) => {
         this.languageControl.patchValue(amazonPolly.language, {
           emitEvent: false,
         });
-
-        this.languageVoiceOptions.set(
-          this.languageVoiceMap.get(amazonPolly.language) ?? []
-        );
 
         this.voiceControl.patchValue(amazonPolly.voice, { emitEvent: false });
         this.poolIdControl.patchValue(amazonPolly.poolId, { emitEvent: false });
@@ -70,20 +51,9 @@ export class AmazonPollyComponent {
 
     this.languageControl.valueChanges
       .pipe(takeUntilDestroyed())
-      .subscribe((language) => {
-        if (language === '') {
-          this.languageVoiceOptions.set([]);
-          return;
-        }
-
-        const options = this.languageVoiceMap.get(language);
-        this.languageVoiceOptions.set(options ?? []);
-        this.voiceControl.patchValue(
-          this.languageVoiceOptions()[0].value ?? ''
-        );
-
-        this.configService.updateAmazonPollyLanguage(language);
-      });
+      .subscribe((language) =>
+        this.configService.updateAmazonPollyLanguage(language)
+      );
 
     this.regionControl.valueChanges
       .pipe(takeUntilDestroyed())
--- src/app/pages/settings/streamelement-tts/streamelement-tts.component.ts
+++ src/app/pages/settings/streamelement-tts/streamelement-tts.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
 import { nonNullFormControl } from 'src/app/shared/utils/form';
 import { ConfigService } from 'src/app/shared/services/config.service';
-import voices from '../../../shared/json/stream-elements-options.json';
+import voices from '../../../shared/json/stream-elements.json';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { MatOptionModule } from '@angular/material/core';
-import { NgFor } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { TtsSelectorComponent } from '../../../shared/components/tts-selector/tts-selector.component';
 
 export interface TTSOption {
   key: string;
@@ -19,39 +15,20 @@ export interface TTSOption {
   templateUrl: './streamelement-tts.component.html',
   styleUrls: ['./streamelement-tts.component.scss'],
   standalone: true,
-  imports: [
-    MatFormFieldModule,
-    MatSelectModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NgFor,
-    MatOptionModule,
-  ],
+  imports: [TtsSelectorComponent],
 })
 export class StreamelementTtsComponent {
-  languageVoiceMap = new Map<string, TTSOption[]>();
-  languageOptions: string[] = [];
-  languageVoiceOptions: TTSOption[] = [];
-
+  readonly voices = voices;
   voiceControl = nonNullFormControl('');
   languageControl = nonNullFormControl('');
 
   constructor(private readonly configService: ConfigService) {
-    for (const voice of voices) {
-      this.languageVoiceMap.set(voice.language, voice.options);
-    }
-
-    this.languageOptions = [...this.languageVoiceMap.keys()];
-
     this.configService.streamElements$
       .pipe(takeUntilDestroyed())
       .subscribe((streamElements) => {
         this.languageControl.patchValue(streamElements.language, {
           emitEvent: false,
         });
-
-        this.languageVoiceOptions =
-          this.languageVoiceMap.get(streamElements.language) ?? [];
 
         this.voiceControl.patchValue(streamElements.voice, {
           emitEvent: false,
@@ -60,23 +37,12 @@ export class StreamelementTtsComponent {
 
     this.languageControl.valueChanges
       .pipe(takeUntilDestroyed())
-      .subscribe((language) => {
-        if (language === '') {
-          this.languageVoiceOptions = [];
-          return;
-        }
-
-        const options = this.languageVoiceMap.get(language);
-        this.languageVoiceOptions = options ?? [];
-        this.voiceControl.patchValue(this.languageVoiceOptions[0].key ?? '');
-
-        this.configService.updateStreamElementsLanguage(language);
-      });
+      .subscribe((language) =>
+        this.configService.updateStreamElementsLanguage(language)
+      );
 
     this.voiceControl.valueChanges
       .pipe(takeUntilDestroyed())
-      .subscribe((voice) => {
-        this.configService.updateVoice(voice);
-      });
+      .subscribe((voice) => this.configService.updateVoice(voice));
   }
 }
