e3b242abd8bf688d29580101f2c06c61124f9b4c
Dylan Warren <dylanwarren19@gmail.com>
2023-07-22 16:21:06 -0500

    update: use formGroup for amazon polly to reduce code

--- src/app/pages/settings/amazon-polly/amazon-polly.component.ts
+++ src/app/pages/settings/amazon-polly/amazon-polly.component.ts
@@ -5,7 +5,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { InputComponent } from '../../../shared/components/input/input.component';
 import { MatOptionModule } from '@angular/material/core';
 import { NgFor } from '@angular/common';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { TtsSelectorComponent } from '../../../shared/components/tts-selector/tts-selector.component';
@@ -30,44 +30,26 @@ export class AmazonPollyComponent {
   regions = [...voices.regions];
   readonly voices = voices.options;
 
-  regionControl = new FormControl(this.regions[0], { nonNullable: true });
-  poolIdControl = new FormControl('', { nonNullable: true });
-  languageControl = new FormControl('', { nonNullable: true });
-  voiceControl = new FormControl('', { nonNullable: true });
+  amazonPollyGroup = new FormGroup({
+    region: new FormControl(this.regions[0], { nonNullable: true }),
+    poolId: new FormControl('', { nonNullable: true }),
+    language: new FormControl('', { nonNullable: true }),
+    voice: new FormControl('', { nonNullable: true }),
+  });
 
   constructor(private readonly configService: ConfigService) {
     this.configService.amazonPolly$
       .pipe(takeUntilDestroyed())
       .subscribe((amazonPolly) => {
-        this.languageControl.patchValue(amazonPolly.language, {
+        this.amazonPollyGroup.setValue(amazonPolly, {
           emitEvent: false,
         });
-
-        this.voiceControl.patchValue(amazonPolly.voice, { emitEvent: false });
-        this.poolIdControl.patchValue(amazonPolly.poolId, { emitEvent: false });
-        this.regionControl.patchValue(amazonPolly.region, { emitEvent: false });
       });
 
-    this.languageControl.valueChanges
+    this.amazonPollyGroup.valueChanges
       .pipe(takeUntilDestroyed())
-      .subscribe((language) =>
-        this.configService.updateAmazonPollyLanguage(language)
+      .subscribe((amazonPolly) =>
+        this.configService.updateAmazonPolly(amazonPolly)
       );
-
-    this.regionControl.valueChanges
-      .pipe(takeUntilDestroyed())
-      .subscribe((region) =>
-        this.configService.updateAmazonPollyRegion(region)
-      );
-
-    this.poolIdControl.valueChanges
-      .pipe(takeUntilDestroyed())
-      .subscribe((poolId) =>
-        this.configService.updateAmazonPollyPoolId(poolId)
-      );
-
-    this.voiceControl.valueChanges
-      .pipe(takeUntilDestroyed())
-      .subscribe((voice) => this.configService.updateAmazonPollyVoice(voice));
   }
 }
