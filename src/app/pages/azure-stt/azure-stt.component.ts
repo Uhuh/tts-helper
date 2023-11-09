5ffcd4c28a51eda4fdf49700c723f09030f21023
Dylan Warren <dylanwarren19@gmail.com>
2023-11-09 00:13:47 -0600

    chore: commit azure changes for clean repo

--- src/app/pages/azure-stt/azure-stt.component.ts
+++ src/app/pages/azure-stt/azure-stt.component.ts
@@ -3,19 +3,21 @@ import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup } from '@angular/forms';
 import { LabelBlockComponent } from '../../shared/components/input-block/label-block.component';
 import { InputComponent } from '../../shared/components/input/input.component';
+import { SelectorComponent } from '../../shared/components/selector/selector.component';
 
 @Component({
   selector: 'app-azure-stt',
   standalone: true,
-  imports: [CommonModule, LabelBlockComponent, InputComponent],
+  imports: [CommonModule, LabelBlockComponent, InputComponent, SelectorComponent],
   templateUrl: './azure-stt.component.html',
   styleUrls: ['./azure-stt.component.scss']
 })
 export class AzureSttComponent {
   azureSettings = new FormGroup({
-    apiKey: new FormControl('', { nonNullable: true })
+    apiKey: new FormControl('', { nonNullable: true }),
+    region: new FormControl('', { nonNullable: true})
   });
-  
+
 }
 
 export default AzureSttComponent;
