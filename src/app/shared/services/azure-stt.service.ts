addd33b43fa07a841cac8b98723483872aa2ba2e
Dylan Warren <dylanwarren19@gmail.com>
2023-11-10 11:52:12 -0600

    update: azure service that will handle audio transfer at some point

---
+++ src/app/shared/services/azure-stt.service.ts
@@ -0,0 +1,8 @@
+﻿import { Injectable } from '@angular/core';
+import sdk from 'microsoft-cognitiveservices-speech-sdk';
+import { ConfigService } from './config.service';
+
+@Injectable()
+export class AzureSttService {
+  constructor(private readonly configService: ConfigService) {}
+}
