86e743256966ccf76df60274993b6e5e22cabd4d
Dylan Warren <dylanwarren19@gmail.com>
2023-11-11 21:06:35 -0600

    update: new component to display text that mimics button styling

---
+++ src/app/shared/components/display-label/display-label.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-label',
+  standalone: true,
+  templateUrl: './display-label.component.html',
+  styleUrl: './display-label.component.scss',
+})
+export class DisplayLabelComponent {
+}
