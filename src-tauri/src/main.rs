33db76f2282cefd06774cf76c85ab0905a95b5f7
Dylan Warren <dylanwarren19@gmail.com>
2023-11-17 20:55:52 -0600

    update: spawn thread for websocket server

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,9 +5,10 @@ mod models;
 mod services;
 
 use anyhow::Context;
-use services::run_auth_server;
+use services::run_websocket_server;
 
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
+use crate::services::run_auth_server;
 
 fn main() -> anyhow::Result<()> {
     Registry::default()
@@ -27,6 +28,7 @@ fn main() -> anyhow::Result<()> {
             // Run auth server
             let handle = app.handle();
             std::thread::spawn(move || run_auth_server(handle));
+            std::thread::spawn(move || run_websocket_server());
 
             Ok(())
         })
