72dfc0a904b6cec430605b3a47d7fab9dd0e101f
Dylan Warren <dylanwarren19@gmail.com>
2023-11-27 14:24:46 -0600

    feat: stream deck support with basic events

---
+++ src/app/shared/services/streamdeck-websocket.interface.ts
@@ -0,0 +1,12 @@
+﻿export enum StreamDeckActions {
+  STT = 'com.tts-helper.tts-helper.stt',
+  TOGGLE_QUEUE = 'com.tts-helper.tts-helper.toggle-queue',
+  REFRESH = 'com.tts-helper.tts-helper.refresh',
+  SKIP = 'com.tts-helper.tts-helper.skip',
+}
+
+export interface StreamDeckEvent {
+  action: string;
+  event: string;
+  data: string;
+}
