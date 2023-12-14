a1c54c1e22ecebd20fc2c9b0a5c1aa08cb739169
Dylan Warren <dylanwarren19@gmail.com>
2023-12-13 22:10:20 -0600

    chore: import settings comp for vstream page

--- src/app/pages/vstream/vstream.component.ts
+++ src/app/pages/vstream/vstream.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth/auth.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @Component({
   selector: 'app-vstream',
   standalone: true,
-  imports: [CommonModule, AuthComponent],
+  imports: [CommonModule, AuthComponent, SettingsComponent],
   templateUrl: './vstream.component.html',
   styleUrl: './vstream.component.scss',
 })
