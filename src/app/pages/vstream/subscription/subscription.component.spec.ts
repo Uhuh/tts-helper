7c2f42a77a290802ec7d2622812b5a2124becc9f
Dylan Warren <dylanwarren19@gmail.com>
2023-12-15 16:24:12 -0600

    feat: add subscription custom message support

---
+++ src/app/pages/vstream/subscription/subscription.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SubscriptionComponent } from './subscription.component';
+
+describe('SubscriptionComponent', () => {
+  let component: SubscriptionComponent;
+  let fixture: ComponentFixture<SubscriptionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SubscriptionComponent],
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(SubscriptionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
