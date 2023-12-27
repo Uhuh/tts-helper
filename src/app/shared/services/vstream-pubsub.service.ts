5014aba8be56c2b0ef86de9966ca1ebdd71c14d3
Dylan Warren <dylanwarren19@gmail.com>
2023-12-26 22:06:51 -0600

    fix: if a command was an empty string it was technically valid, check for empty string

--- src/app/shared/services/vstream-pubsub.service.ts
+++ src/app/shared/services/vstream-pubsub.service.ts
@@ -254,12 +254,12 @@ export class VStreamPubSubService {
       .pipe(
         first(),
         map(commands => commands.find(c => text.startsWith(c.command))),
-        filter(command => !!command),
+        filter(command => !!command && !!command.command),
       )
       .subscribe(command => {
         this.logService.add(`User "${chatter.displayName}" ran a chat command "${command?.command}"`, 'info', 'VStreamPubSub.handleChatMessage');
 
-        this.vstreamService.sendCommandResponse(permissions, command);
+        this.vstreamService.sendCommandResponse(permissions, command, text);
       });
   }
 }
