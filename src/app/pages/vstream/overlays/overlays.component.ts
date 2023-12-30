5b2c5ae0d1a3163ef68bfb704b38531c67aa6c5d
Dylan Warren <dylanwarren19@gmail.com>
2023-12-29 19:20:16 -0600

    update: allow users to create and test overlays from main overlay page

--- src/app/pages/vstream/overlays/overlays.component.ts
+++ src/app/pages/vstream/overlays/overlays.component.ts
@@ -2,35 +2,51 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { LabelBlockComponent } from '../../../shared/components/input-block/label-block.component';
-import { MatDialog } from '@angular/material/dialog';
-import { WidgetDialogComponent } from './widget-dialog/widget-dialog.component';
 import { VStreamService } from '../../../shared/services/vstream.service';
-import { VStreamWidget } from '../../../shared/state/vstream/vstream.feature';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EditWidgetComponent } from './edit-widget/edit-widget.component';
+import { take } from 'rxjs';
+import { generateBrowserSource } from './utils/generateBrowserSource';
+import { VStreamPubSubService } from '../../../shared/services/vstream-pubsub.service';
 
 @Component({
   selector: 'app-overlays',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, LabelBlockComponent],
+  imports: [CommonModule, ButtonComponent, LabelBlockComponent, EditWidgetComponent],
   templateUrl: './overlays.component.html',
   styleUrl: './overlays.component.scss',
 })
 export class OverlaysComponent {
   widgets$ = this.vstreamService.widgets$;
 
-  constructor(private readonly dialog: MatDialog, private readonly vstreamService: VStreamService) {}
+  constructor(
+    private readonly vstreamService: VStreamService,
+    private readonly vstreamPubsub: VStreamPubSubService,
+    private readonly snackbar: MatSnackBar,
+  ) {}
 
-  openDialog(widget?: VStreamWidget) {
-    const dialogRef = this.dialog.open(WidgetDialogComponent, {
-      data: widget,
-      width: '350',
-    });
+  createWidget() {
+    this.vstreamService.createWidget();
+  }
+  
+  test() {
+    this.vstreamPubsub.testOverlays();
+  }
 
-    dialogRef.afterClosed().subscribe((widget: VStreamWidget | Omit<VStreamWidget, 'id'>) => {
-      if ('id' in widget && widget.id) {
-        this.vstreamService.updateWidget(widget);
-      } else {
-        this.vstreamService.createWidget(widget);
-      }
-    });
+  downloadBrowserSource() {
+    this.widgets$
+      .pipe(take(1))
+      .subscribe(widgets => {
+        const data = 'data:text/html;charset=utf-8,' + encodeURIComponent(generateBrowserSource(widgets));
+
+        const anchor = document.createElement('a');
+        anchor.download = `tts-helper-vstream-overlays.html`;
+        anchor.href = data;
+        anchor.click();
+
+        this.snackbar.open('Successfully downloaded VStream overlays.', 'Dismiss', {
+          panelClass: 'notification-success',
+        });
+      });
   }
 }
