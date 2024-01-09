c974a172d38f5dfefcc011bf06523aced23503ff
Dylan Warren <dylanwarren19@gmail.com>
2024-01-09 13:07:37 -0600

    update: select component to use generic wording and take types for the options

--- src/app/shared/components/selector/selector.component.ts
+++ src/app/shared/components/selector/selector.component.ts
@@ -5,9 +5,9 @@ import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
-type Option = {
+export type Option<T> = {
   displayName: string;
-  value: string | number;
+  value: T;
 };
 
 @Component({
@@ -17,8 +17,8 @@ type Option = {
   templateUrl: './selector.component.html',
   styleUrls: ['./selector.component.scss'],
 })
-export class SelectorComponent {
-  @Input({ required: true }) options!: Option[];
+export class SelectorComponent<T> {
+  @Input({ required: true }) options!: Option<T>[];
   @Input({ required: true }) control!: FormControl;
   @Input({ required: true }) placeholder!: string;
 }
