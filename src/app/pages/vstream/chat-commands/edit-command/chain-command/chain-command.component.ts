5418bc44d10cb71801ea6484db11c63b59377624
Dylan Warren <dylanwarren19@gmail.com>
2024-01-09 12:55:34 -0600

    feat: chain command editing and creating

---
+++ src/app/pages/vstream/chat-commands/edit-command/chain-command/chain-command.component.ts
@@ -0,0 +1,33 @@
+import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VStreamService } from '../../../../../shared/services/vstream.service';
+import { Commands } from '../../../../../shared/services/command.interface';
+import { Option } from '../../../../../shared/components/selector/selector.component';
+import { map, Observable } from 'rxjs';
+import { ButtonComponent } from '../../../../../shared/components/button/button.component';
+import { LabelBlockComponent } from '../../../../../shared/components/input-block/label-block.component';
+import { EditChainCommandComponent } from './edit-chain-command/edit-chain-command.component';
+
+@Component({
+  selector: 'app-chain-command',
+  standalone: true,
+  imports: [CommonModule, ButtonComponent, LabelBlockComponent, EditChainCommandComponent],
+  templateUrl: './chain-command.component.html',
+  styleUrl: './chain-command.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ChainCommandComponent {
+  private readonly vstreamService = inject(VStreamService);
+
+  readonly options$: Observable<Option<string>[]> = this.vstreamService.commands$
+    .pipe(map(commands => commands.filter(c => c.id !== this.command.id).map(c => ({
+      displayName: c.command,
+      value: c.id,
+    }))));
+
+  @Input({ required: true }) command!: Commands;
+
+  createChainCommand() {
+    this.vstreamService.createChainCommand(this.command.id, null);
+  }
+}
---
+++ src/app/pages/vstream/chat-commands/edit-command/chain-command/edit-chain-command/edit-chain-command.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChainCommand } from '../../../../../../shared/services/command.interface';
+import { ButtonComponent } from '../../../../../../shared/components/button/button.component';
+import { VStreamService } from '../../../../../../shared/services/vstream.service';
+import { map, Observable } from 'rxjs';
+import { Option, SelectorComponent } from '../../../../../../shared/components/selector/selector.component';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-chain-command',
+  standalone: true,
+  imports: [CommonModule, ButtonComponent, SelectorComponent],
+  templateUrl: './edit-chain-command.component.html',
+  styleUrl: './edit-chain-command.component.scss',
+})
+export class EditChainCommandComponent implements OnInit {
+  private readonly vstreamService = inject(VStreamService);
+
+  readonly chainCommandControl = new FormControl<string | null>(null);
+  readonly options$: Observable<Option<string>[]> = this.vstreamService.commands$
+    .pipe(map(commands => commands.filter(c => c.id !== this.parentCommandID)
+      .map(c => ({
+          displayName: c.command,
+          value: c.id,
+        }),
+      )),
+    );
+
+  @Input({ required: true }) parentCommandID!: string;
+  @Input({ required: true }) chainCommand!: ChainCommand;
+
+  ngOnInit() {
+    this.chainCommandControl.setValue(this.chainCommand.chainCommandID);
+
+    this.chainCommandControl.valueChanges
+      .subscribe(id => {
+        this.vstreamService.updateChainCommand(this.parentCommandID, this.chainCommand.id, id);
+      });
+  }
+
+  deleteChainCommand() {
+    this.vstreamService.deleteChainCommand(this.parentCommandID, this.chainCommand.id);
+  }
+}
