d9b8831db04a67c72a09aabc59171250b884bd9a
Dylan Warren <dylanwarren19@gmail.com>
2024-01-30 16:24:07 -0600

    update: remove coverage checker for now

--- karma.conf.js
+++ karma.conf.js
@@ -31,14 +31,6 @@ module.exports = function (config) {
         { type: 'html' },
         { type: 'text-summary' }
       ],
-      check: {
-        global: {
-          statements: 80,
-          branches: 80,
-          functions: 80,
-          lines: 80
-        }
-      }
     },
     reporters: ['progress', 'kjhtml'],
     browsers: ['Chrome'],
