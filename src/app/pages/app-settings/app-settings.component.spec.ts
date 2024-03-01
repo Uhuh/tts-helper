cda8b782cc0858a6d27f2986a0384809fe15ee7c
Dylan Warren <dylanwarren19@gmail.com>
2024-02-29 20:39:51 -0600

    chore: basic test providers

--- src/app/pages/app-settings/app-settings.component.spec.ts
+++ src/app/pages/app-settings/app-settings.component.spec.ts
@@ -1,17 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppSettingsComponent } from './app-settings.component';
+import { AppSettingsService } from '../../shared/services/app-settings.service';
+import { Subject } from 'rxjs';
+import { AppSettingsFeatureState } from '../../shared/state/app-settings/app-settings.feature';
 
 describe('AppSettingsComponent', () => {
   let component: AppSettingsComponent;
   let fixture: ComponentFixture<AppSettingsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppSettingsComponent]
-    })
-    .compileComponents();
-    
+  let appSettingsServiceStub: jasmine.SpyObj<AppSettingsService>;
+
+  let connectionsSubject: Subject<AppSettingsFeatureState['connections']>;
+
+  beforeEach(() => {
+    connectionsSubject = new Subject();
+    appSettingsServiceStub = jasmine.createSpyObj('AppSettingsService', [''], {
+      connections$: connectionsSubject,
+    });
+
+    TestBed.overrideComponent(AppSettingsComponent, {
+      set: {
+        providers: [
+          { provide: AppSettingsService, useValue: appSettingsServiceStub },
+        ],
+      },
+    });
+
     fixture = TestBed.createComponent(AppSettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
