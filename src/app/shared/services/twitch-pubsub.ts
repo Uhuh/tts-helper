9dfb293551553d5fad6ad0f7b4dfd7b447e98aa9
Dylan Warren <dylanwarren19@gmail.com>
2024-04-26 22:43:57 -0500

    update: more rxjs based functions and new follower event listener

--- src/app/shared/services/twitch-pubsub.ts
+++ src/app/shared/services/twitch-pubsub.ts
@@ -1,28 +1,26 @@
 ﻿import { StaticAuthProvider } from '@twurple/auth';
-import { inject, Injectable, OnDestroy } from '@angular/core';
+import { DestroyRef, inject, Injectable } from '@angular/core';
 import { TwitchService } from './twitch.service';
-import { combineLatest } from 'rxjs';
+import { combineLatest, filter, first, map } from 'rxjs';
 import { ApiClient } from '@twurple/api';
 import { EventSubWsListener } from '@twurple/eventsub-ws';
 import { ChatClient, ChatUser } from '@twurple/chat';
 import { AudioService } from './audio.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { TwitchCheer, TwitchGiftSub, TwitchRedeem, TwitchSub, TwitchSubMessage } from './twitch-pubsub.interface';
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { LogService } from './logs.service';
 import { OpenAIService } from './openai.service';
-import { GptSettingsState } from '../state/openai/openai.feature';
-import { TwitchSettingsState } from '../state/twitch/twitch.feature';
 import { ChatService } from './chat.service';
 
 @Injectable()
-export class TwitchPubSub implements OnDestroy {
+export class TwitchPubSub {
   private readonly twitchService = inject(TwitchService);
   private readonly audioService = inject(AudioService);
   private readonly logService = inject(LogService);
   private readonly openaiService = inject(OpenAIService);
   private readonly chatService = inject(ChatService);
-  private readonly snackbar = inject(MatSnackBar);
+
+  private readonly destroyRef = inject(DestroyRef);
 
   // TTS Helper client ID. This isn't a sensitive code.
   private readonly clientId = 'fprxp4ve0scf8xg6y48nwcq1iogxuq';
@@ -30,35 +28,18 @@ export class TwitchPubSub implements OnDestroy {
   listener: EventSubWsListener | null = null;
   chat: ChatClient | null = null;
 
-  readonly bitInfo = toSignal(this.twitchService.bitInfo$);
-  readonly redeemInfo = toSignal(this.twitchService.redeemInfo$);
-  readonly subsInfo = toSignal(this.twitchService.subsInfo$);
+  private readonly bitsSettings$ = this.twitchService.bitInfo$;
+  private readonly redeemsSettings$ = this.twitchService.redeemInfo$;
 
-  // Normal TTS chat command
-  twitchSettings!: TwitchSettingsState;
+  private readonly subscriptions$ = this.twitchService.subscriptions$;
+  private readonly giftSettings$ = this.subscriptions$.pipe(map(s => s.gift));
+  private readonly renewSettings$ = this.subscriptions$.pipe(map(s => s.renew));
+  private readonly follower$ = this.twitchService.follower$;
 
-  // ChatGPT Settings
-  gptSettings!: GptSettingsState;
-
-  // Twurple doesn't expose the listener type for some reason.
-  onMessageListener?: any;
-  redeemListener?: any;
-  bitsListener?: any;
-
-  // Subs listener
-  subsListener?: any;
-  giftSubListener?: any;
-  subMessageListener?: any;
+  twitchSettings$ = this.twitchService.settings$;
+  gptSettings$ = this.openaiService.settings$;
 
   constructor() {
-    this.openaiService.settings$
-      .pipe(takeUntilDestroyed())
-      .subscribe(settings => this.gptSettings = settings);
-
-    this.twitchService.settings$
-      .pipe(takeUntilDestroyed())
-      .subscribe(settings => this.twitchSettings = settings);
-
     combineLatest([
       this.twitchService.token$,
       this.twitchService.channelInfo$,
@@ -69,6 +50,8 @@ export class TwitchPubSub implements OnDestroy {
         if (!token || !channelInfo.channelId) {
           return;
         }
+
+        const { channelId } = channelInfo;
 
         this.logService.add(`Attempting to connect to Twitch chat.`, 'info', 'TwitchPubSub.constructor');
 
@@ -83,47 +66,15 @@ export class TwitchPubSub implements OnDestroy {
           channels: [channelInfo.username],
         });
 
-        this.chat.connect().catch((e) => {
-          this.logService.add(`Failed to connect to Twitch chat.\n${e}`, 'error', 'TwitchPubSub.chat.connect');
+        this.chat.connect();
 
-          this.snackbar.open(
-            'Oops! We encountered an error connecting to Twitch.',
-            'Dismiss',
-            {
-              panelClass: 'notification-error',
-            },
-          );
-        });
-
-        this.redeemListener = this.listener.onChannelRedemptionAdd(
-          channelInfo.channelId ?? '',
-          (c) => this.onRedeem(c),
-        );
-
-        this.bitsListener = this.listener.onChannelCheer(
-          channelInfo.channelId ?? '',
-          (cheer) => this.onBits(cheer),
-        );
-
-        this.subsListener = this.listener.onChannelSubscription(
-          channelInfo.channelId ?? '',
-          (sub) => this.onSub(sub),
-        );
-
-        this.giftSubListener = this.listener.onChannelSubscriptionGift(
-          channelInfo.channelId ?? '',
-          (gift) => this.onGiftSub(gift),
-        );
-
-        this.subMessageListener = this.listener.onChannelSubscriptionMessage(
-          channelInfo.channelId ?? '',
-          (message) => this.onSubMessage(message),
-        );
-
-        this.onMessageListener = this.chat.onMessage((_, _username, text, msg) => {
-            this.onMessage(msg.userInfo, text);
-          },
-        );
+        this.chat.onMessage((_, _u, text, msg) => this.onMessage(msg.userInfo, text));
+        this.listener.onChannelCheer(channelId, (cheer) => this.onBits(cheer));
+        this.listener.onChannelFollow(channelId, channelId, (user) => this.onFollow(user));
+        this.listener.onChannelRedemptionAdd(channelId, (c) => this.onRedeem(c));
+        this.listener.onChannelSubscription(channelId, (sub) => this.onSub(sub));
+        this.listener.onChannelSubscriptionGift(channelId, (gift) => this.onGiftSub(gift));
+        this.listener.onChannelSubscriptionMessage(channelId, (message) => this.onSubMessage(message));
       });
   }
 
@@ -141,122 +92,150 @@ export class TwitchPubSub implements OnDestroy {
       'twitch',
     );
 
-    const { randomChance } = this.twitchSettings;
-
-    if (!playedMessage && randomChance) {
-      this.chatService.randomChance(
-        {
-          text,
-          displayName: user.displayName,
-        },
-        randomChance,
-        this.gptSettings.enabled,
-        'twitch',
-      );
+    // If the users message was a chat command, we don't want to potentially read it twice.
+    if (playedMessage) {
+      return;
     }
+
+    combineLatest([this.twitchSettings$, this.gptSettings$])
+      .pipe(first(), takeUntilDestroyed(this.destroyRef))
+      .subscribe(([twitch, gpt]) => {
+        const { randomChance } = twitch;
+
+        this.chatService.randomChance(
+          {
+            text,
+            displayName: user.displayName,
+          },
+          randomChance,
+          gpt.enabled,
+          'twitch',
+        );
+      });
+  }
+
+  onFollow(user: { userName: string, userDisplayName: string }) {
+    this.follower$
+      .pipe(first(), filter(f => f.enabled), takeUntilDestroyed(this.destroyRef))
+      .subscribe(follower => {
+        const parsedInput = follower.customMessage
+          .replaceAll(/{username}/g, user.userName);
+
+        this.audioService.playTts(
+          parsedInput ?? '',
+          user.userDisplayName,
+          'twitch',
+          999,
+        );
+      });
   }
 
   onSubMessage(message: TwitchSubMessage) {
-    if (!this.subsInfo()?.enabled || !message.messageText || !this.subsInfo()) {
-      return;
-    }
-
-    this.audioService.playTts(
-      message.messageText,
-      message.userDisplayName,
-      'twitch',
-      this.subsInfo()?.subCharacterLimit ?? 300,
-    );
+    this.renewSettings$
+      .pipe(first(), filter(renew => renew.enabled && !!message.messageText), takeUntilDestroyed(this.destroyRef))
+      .subscribe(renew => {
+        this.audioService.playTts(
+          message.messageText,
+          message.userDisplayName,
+          'twitch',
+          renew.characterLimit,
+        );
+      });
   }
 
-  onGiftSub(gift: TwitchGiftSub) {
-    if (!this.subsInfo()?.enabled) {
-      return;
-    }
+  onGiftSub(gifter: TwitchGiftSub) {
+    this.giftSettings$
+      .pipe(first(), filter(gift => gift.enabled), takeUntilDestroyed())
+      .subscribe(gift => {
+        const parsedInput = gift.customMessage
+          .replaceAll(/{username}/g, gifter.gifterDisplayName)
+          .replaceAll(/{amount}/g, `${gifter.amount}`)
+          .replaceAll(/{cumulative}/g, `${gifter.cumulativeAmount ?? `That's a secret!`}`);
 
-    const parsedInput = this.subsInfo()?.giftMessage
-      .replaceAll(/{username}/g, gift.gifterDisplayName)
-      .replaceAll(/{amount}/g, `${gift.amount}`);
-
-    this.audioService.playTts(
-      parsedInput ?? '',
-      gift.gifterDisplayName,
-      'twitch',
-      999,
-    );
+        this.audioService.playTts(
+          parsedInput ?? '',
+          gifter.gifterDisplayName,
+          'twitch',
+          999,
+        );
+      });
   }
 
-  /**
-   * @TODO - Figure out if on subs need to trigger TTS or have custom messages
-   */
   onSub(sub: TwitchSub) {
     /**
      * Ignore users that received a gift sub
      */
-    if (!this.subsInfo()?.enabled || sub.isGift) {
+    if (sub.isGift) {
       return;
     }
 
-    this.audioService.playTts(
-      'Thanks for the sub',
-      sub.userDisplayName,
-      'twitch',
-      999,
-    );
+    this.renewSettings$
+      .pipe(first(), filter(settings => settings.enabled))
+      .subscribe(renew => {
+        const parsedInput = renew.customMessage
+          .replaceAll(/{username}/g, sub.userName)
+          .replaceAll(/{tier}/g, `${sub.tier}`);
+
+        this.audioService.playTts(
+          parsedInput,
+          sub.userDisplayName,
+          'twitch',
+          999,
+        );
+      });
   }
 
   onBits(cheer: TwitchCheer) {
-    if (
-      !this.bitInfo()?.enabled ||
-      cheer.bits < (this.bitInfo()?.minBits ?? 100) ||
-      (this.bitInfo()?.exact && cheer.bits !== this.bitInfo()?.minBits)
-    ) {
-      return;
-    }
+    this.bitsSettings$
+      .pipe(
+        first(),
+        filter(
+          settings =>
+            settings.enabled &&
+            settings.exact
+              ? cheer.bits === settings.minBits
+              : settings.minBits <= cheer.bits,
+        ),
+      )
+      .subscribe(bits => {
+        const cleanedInput = cheer.message.replaceAll(/Cheer[0-9]+/g, '');
 
-    const cleanedInput = cheer.message.replaceAll(/Cheer[0-9]+/g, '');
-
-    this.audioService.playTts(
-      cleanedInput,
-      cheer.userDisplayName ?? '[ANONYMOUS]',
-      'twitch',
-      this.bitInfo()?.bitsCharacterLimit ?? 300,
-    );
+        this.audioService.playTts(
+          cleanedInput,
+          cheer.userDisplayName ?? '[ANONYMOUS]',
+          'twitch',
+          bits.bitsCharacterLimit,
+        );
+      });
   }
 
   onRedeem(redeem: TwitchRedeem) {
     const { rewardId, userDisplayName, input } = redeem;
 
-    // Normal TTS.
-    if (
-      rewardId === this.redeemInfo()?.redeem &&
-      this.redeemInfo()?.enabled
-    ) {
-      this.audioService.playTts(
-        input || 'Haha! Someone forgot to say something.',
-        userDisplayName,
-        'twitch',
-        this.redeemInfo()?.redeemCharacterLimit ?? 100,
-      );
-    }
+    combineLatest([
+      this.redeemsSettings$,
+      this.gptSettings$,
+    ])
+      .pipe(first(), filter(([redeems, gpt]) => redeems.enabled || gpt.enabled))
+      .subscribe(([redeems, gpt]) => {
+        // Normal TTS.
+        if (redeems.enabled && rewardId === redeems.redeem) {
+          this.audioService.playTts(
+            input || 'Haha! Someone forgot to say something.',
+            userDisplayName,
+            'twitch',
+            redeems.redeemCharacterLimit,
+          );
+        }
 
-    // If the streamer has GPT enabled, forward all TTS to ChatGPT.
-    if (this.gptSettings?.enabled && this.redeemInfo()?.gptRedeem === rewardId) {
-      this.openaiService.generateOpenAIResponse(userDisplayName, input);
-    }
+        // If the streamer has GPT enabled, forward all TTS to ChatGPT.
+        if (gpt.enabled && redeems.gptRedeem === rewardId) {
+          this.openaiService.generateOpenAIResponse(userDisplayName, input);
+        }
+      });
   }
 
   cleanupListeners() {
-    this.chat?.removeListener(this.onMessageListener);
-    this.listener?.removeListener(this.redeemListener);
-    this.listener?.removeListener(this.bitsListener);
-    this.listener?.removeListener(this.subsListener);
-    this.listener?.removeListener(this.giftSubListener);
-    this.listener?.removeListener(this.subMessageListener);
     this.listener?.stop();
-  }
-
-  ngOnDestroy() {
-    this.cleanupListeners();
   }
 }
