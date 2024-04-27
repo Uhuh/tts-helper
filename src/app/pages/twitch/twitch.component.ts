a8eb0aa7425ec9aafb3772bc88a487271b3e8976
Dylan Warren <dylanwarren19@gmail.com>
2024-04-26 22:41:02 -0500

    update: put each type in its own tab for visual clarity

--- src/app/pages/twitch/twitch.component.ts
+++ src/app/pages/twitch/twitch.component.ts
@@ -4,13 +4,15 @@ import { BitsComponent } from './bits/bits.component';
 import { RedeemsComponent } from './redeems/redeems.component';
 import { AuthComponent } from './auth/auth.component';
 import { SettingsComponent } from './settings/settings.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FollowersComponent } from './followers/followers.component';
 
 @Component({
   selector: 'app-twitch',
   templateUrl: './twitch.component.html',
   styleUrls: ['./twitch.component.scss'],
   standalone: true,
-  imports: [AuthComponent, RedeemsComponent, BitsComponent, SubsComponent, SettingsComponent],
+  imports: [AuthComponent, RedeemsComponent, BitsComponent, SubsComponent, SettingsComponent, MatTabsModule, FollowersComponent],
 })
 export class TwitchComponent {}
 
