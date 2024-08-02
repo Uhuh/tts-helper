6b85e34b3ebc7111fb3765f753d0118e2a72aef9
Dylan Warren <dylanwarren19@gmail.com>
2024-08-02 14:10:59 -0500

    update: expose tauri commands for monitors

--- plugins/tauri-plugin-playback/build.rs
+++ plugins/tauri-plugin-playback/build.rs
@@ -1,5 +1,5 @@
 const COMMANDS: &[&str] = &[
-    "list_output_devices", "set_output_device", "play_audio", "set_playback_state", "set_output_volume", "toggle_pause", "set_audio_state", "list_audio"
+    "list_output_devices", "list_monitors", "snapshot_monitor", "set_output_device", "play_audio", "set_playback_state", "set_output_volume", "toggle_pause", "set_audio_state", "list_audio"
 ];
 
 fn main() {
