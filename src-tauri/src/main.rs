f58eed70fc42d42041e7a676f7e9b8309262d9de
Dylan Warren <dylanwarren19@gmail.com>
2024-08-03 14:07:29 -0500

    update: revert main back to just tauri, ignoring mobile support for now

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,42 @@
 ﻿#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-  app_lib::run();
+mod models;
+mod services;
+
+use anyhow::Context;
+
+use services::tts_listener::run_tts_server;
+use crate::services::{run_auth_server, start_ws_server};
+
+pub fn main() -> anyhow::Result<()> {
+    let obs_port = "37891";
+    let streamdeck_port = "17448";
+
+    let builder = tauri::Builder::default();
+
+    builder
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_updater::Builder::default().build())
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+		.plugin(tauri_plugin_devtools::init())
+        .plugin(tauri_plugin_global_shortcut::Builder::default().build())
+        .plugin(tauri_plugin_playback::init()?)
+        .setup(|app| {
+            // Run auth server
+            let handle = app.handle().clone();
+            std::thread::spawn(move || run_auth_server(handle));
+            let handle = app.handle().clone();
+            std::thread::spawn(move || run_tts_server(handle));
+
+            tauri::async_runtime::spawn(start_ws_server(obs_port));
+            tauri::async_runtime::spawn(start_ws_server(streamdeck_port));
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .context("error while running tauri application")?;
+
+    Ok(())
 }
